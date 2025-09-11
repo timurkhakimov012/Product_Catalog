72c781cff80e3c5f015be5ebb06932cfea066b3e
Timerkhan <khakimov012@yandex.ru>
2025-09-11 14:10:17 +0300

    starting markup for header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import styles from "./paje.module.css";
+import Logo from "../components/logo/Logo";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Home page</h1>
+    <div className="container">
+
+      <header className={styles["header"]}>
+        <div className={styles["header__logo"]}>
+          <Logo/>
+        </div>
+
+        <div className={styles["header__nav"]}>Nav</div>
+        <div className={styles["header__search"]}>Search</div>
+        <div className={styles["header__user-menu"]}>User Menu</div>
+      </header>
+
+      <main className={styles["main"]}>
+         <div className={styles["main__sideBar"]}>sideBar</div>
+         <div className={styles["main__content"]}>Content</div>
+      </main>
+
+      <footer>
+        <h3>footer</h3>
+      </footer>
+
     </div>
   );
 }
---
+++ components/logo/Logo.tsx
@@ -0,0 +1,15 @@
+import styles from "./style.module.css";
+import Image from "next/image";
+
+const Logo = () => {
+    return (
+        <div className={styles["logo"]}>
+            <div className={styles["logo__image"]}>
+                <Image src ="/logo.svg" alt="logo" width={32} height={32}/>
+            </div>
+            <div className={styles["logo__text"]}>Ecommerce</div>
+        </div>
+    );
+}
+
+export default Logo;
